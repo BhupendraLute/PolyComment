c60ae629807ee01c776f24ffce92026310a17f3c
Bhupendra Lute <bhupendralute1234@gmail.com.com>
2026-02-04 00:21:41 +0530

    feat: Migrate extension build process from direct TypeScript compilation to Webpack for bundling.
    add: working extension build to releases
    
    remove: extension builds with activation event problems

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  target: 'node', // extensions run in a node context
+  mode: 'none', // this leaves the code readable while we debug
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs',
+    devtoolModuleFilenameTemplate: '..[resource-path]'
+  },
+  devtool: 'nosources-source-map',
+  externals: {
+    vscode: 'commonjs vscode' // the vscode-module is provided by the editor
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    mainFields: ['module', 'main'] // prefer ESM versions of dependencies
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
